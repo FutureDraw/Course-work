94a920102816e84b751c1e4b2a508c29ac4c5927
Aleksandr Avdeev <avdeevchope@gmail.com>
2023-12-18 16:32:42 +0300

    Изменение интерфейса

--- Main.py
+++ Main.py
@@ -12,6 +12,9 @@ class VideoConverterApp:
         self.source_file_label = tk.Label(master, text="Выберите видеофайл:", bg="#333333", fg="white")
         self.source_file_label.grid(row=0, column=0, padx=2, pady=10, sticky="w")
 
+        self.source_file_entry = tk.Entry(master, width=30)
+        self.source_file_entry.grid(row=0, column=1, padx=0, pady=10, sticky="ew")
+
         self.browse_button = tk.Button(master, text="Обзор", bg="white", command=self.browse_file)
         self.browse_button.grid(row=0, column=2, padx=10, pady=10, sticky="e")
 
